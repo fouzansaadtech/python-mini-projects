6a7b96802d8743548468061c3e632d9395e75a50
Fouzan Saad H <fouzansaad.tech@gmail.com>
2026-07-02 12:38:32 +0530

    Login Authentication

---
+++ 03_login_authentication.py
@@ -0,0 +1,8 @@
+# Task 8:Login Authentication
+
+Username=input("Username:")
+Password=int(input("Password:"))
+if(Username=="admin" and Password==1234):
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
