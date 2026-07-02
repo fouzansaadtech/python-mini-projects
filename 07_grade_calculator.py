acf054f67b2cfe05ffe439f522f6cb7a167bd981
Fouzan Saad H <fouzansaad.tech@gmail.com>
2026-07-02 12:56:20 +0530

    Grade Calculator

---
+++ 07_grade_calculator.py
@@ -0,0 +1,11 @@
+# Task 6:Grade Calculator
+
+Marks=int(input("Marks:"))
+if(Marks>=90):
+    print("Grade A")
+elif(Marks>=75):
+    print("Grade B")
+elif(Marks>=50):
+    print("Grade C")
+else:
+    print("Grade D")
